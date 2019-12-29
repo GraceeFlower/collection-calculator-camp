9d8b32ab541e64e1ade7b6eaa532ca5c258f3f28
GraceeFlower <1491644103@qq.com>
2019-12-29 21:55:40 +0800

    [WangMin] feat-6: get letter interval of 1

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  var res = [];
+  if (number_a < number_b) {
+    for(var item = number_a; item <= number_b; item++) {
+      res.push(String.fromCharCode(96 + item));
+    } 
+  } else if (number_a > number_b) {
+    for(var item = number_a; item >= number_b; item--) {
+      res.push(String.fromCharCode(96 + item));
+    }
+  } else {
+      res.push(String.fromCharCode(96 + number_a));
+  }
+  return res;
 }
 
 module.exports = get_letter_interval;

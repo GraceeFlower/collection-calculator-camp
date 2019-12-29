5c01dd373ca7db8823804fecbad2307808aa0394
GraceeFlower <1491644103@qq.com>
2019-12-29 22:31:54 +0800

    [WangMin] feat-2: use filter to choose the multiples of 3

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //在这里写入代码
+  var res = collection.filter((item) => !(item % 3));
+  return res;
 }
 
 module.exports = choose_multiples_of_three;

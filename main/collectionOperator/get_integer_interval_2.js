8ddcfe60a1601529aa3af994c339c93eb0e19e3e
GraceeFlower <1491644103@qq.com>
2019-12-29 18:15:30 +0800

    [WangMin] feat-3: get integer inteval about mixed evens & odds

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,23 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var res = [];
+  if (number_a < number_b) {
+    var item = number_a % 2 ? number_a + 1 : number_a;
+    while(item <= number_b) {
+      res.push(item);
+      item += 2;
+    }
+  } else if(number_a > number_b) {
+    var item = number_a % 2 ? number_a - 1 : number_a;
+    while(item >= number_b) {
+      res.push(item);
+      item -= 2;
+    }
+  } else {
+    number_a % 2 ? [] : res.push(number_a);
+  }
+  return res;
 }
 
 module.exports = get_integer_interval_2;

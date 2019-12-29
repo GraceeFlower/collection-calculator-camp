693825af93753dd6a78f6b3a293a4049db3a4b52
GraceeFlower <1491644103@qq.com>
2019-12-29 21:52:00 +0800

    [WangMin] feat-6: get letter interval of 2

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,31 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var res = [];
+  if (number_a < number_b) {
+    for(var item = number_a; item <= number_b; item++) {
+      res.push(generateString(item));
+    } 
+  } else if (number_a > number_b) {
+    for(var item = number_a; item >= number_b; item--) {
+      res.push(generateString(item));
+    }
+  } else {
+      res.push(generateString(number_a));
+  }
+  return res;
+}
+
+function generateString(index) {
+  var excess = Math.floor((index - 1) / 26);
+  var basic = 96 + index;
+  var former = 96 + excess;
+  var latter = 96 + index - excess * 26;
+  if (!excess) {
+    return String.fromCharCode(basic);
+  } else {
+    return String.fromCharCode(former) + String.fromCharCode(latter);
+  }
 }
 
 module.exports = get_letter_interval_2;

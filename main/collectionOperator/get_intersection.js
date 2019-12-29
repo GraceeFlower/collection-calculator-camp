99c4f3a3827f3af7151768209f373d95b30bf9d7
GraceeFlower <1491644103@qq.com>
2019-12-29 19:16:14 +0800

    [WangMin] feat-5: get intersection about two same-length arrays

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,18 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  var res = [];
+  for(var bItem = 0; bItem < collection_b.length; bItem++) {
+    for(var aItem = 0; aItem < collection_a.length;) {
+      if(collection_a[aItem] === collection_b[bItem]) {
+        res.push(collection_b[bItem]);
+        break;
+      } else {
+        aItem++;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = get_intersection;

2d8d9854f47c8d730018eacc43f1524007376dce
GraceeFlower <1491644103@qq.com>
2019-12-29 21:59:49 +0800

    [WangMin] fix-7: seperate the generation & printing

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -4,16 +4,20 @@ function get_letter_interval(number_a, number_b) {
   var res = [];
   if (number_a < number_b) {
     for(var item = number_a; item <= number_b; item++) {
-      res.push(String.fromCharCode(96 + item));
+      res.push(generateString(item));
     } 
   } else if (number_a > number_b) {
     for(var item = number_a; item >= number_b; item--) {
-      res.push(String.fromCharCode(96 + item));
+      res.push(generateString(item));
     }
   } else {
-      res.push(String.fromCharCode(96 + number_a));
+    res.push(generateString(number_a));
   }
   return res;
+}
+
+function generateString(item) {
+  return String.fromCharCode(96 + item);
 }
 
 module.exports = get_letter_interval;

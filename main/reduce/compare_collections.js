7ea56bc344468257b01d2a62bec69d24ba47e6a2
GraceeFlower <1491644103@qq.com>
2019-12-30 14:47:13 +0800

    [WangMin] feat-3: use reduce to judge if two arrays equal

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  var res = true;
+  if (collection_a.length === collection_b.length) {
+    collection_a.reduce(function (pre, cur, index, array) {
+      if (array[index] !== collection_b[index]) {
+        res = false;
+      }
+    });
+  } else {
+    res = false;
+  }
+  return res;
 }
 
 module.exports = compare_collections;
-
-

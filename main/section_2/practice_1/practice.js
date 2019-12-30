5e0d8c7a8ae42bfbf5ed3045fd2e5cc8a8692759
GraceeFlower <1491644103@qq.com>
2019-12-30 20:20:27 +0800

    [WangMin] feat-1: solve the first collecting the same elements object in section_2

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,26 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var results = [];
+  collection.filter(function (value, index, array) {
+    var item = checkItem(results, value);
+    if (item !== "") {
+      results[item].count++;
+    } else {
+      var result = {};
+      result.key = value;
+      result.count = 1;
+      results.push(result);
+    }
+  })
+  return results;
+}
+
+function checkItem(arr, element) {
+  for(var item = 0; item < arr.length; item++) {
+    if(arr[item].key === element) {
+      return item;
+    }
+  }
+  return ""; 
 }
 
 module.exports = count_same_elements;

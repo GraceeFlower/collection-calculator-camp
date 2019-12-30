c6ea385b1941276c6542d8161fccdf1bbd80d3d0
GraceeFlower <1491644103@qq.com>
2019-12-30 21:54:14 +0800

    [WangMin] feat-2: solve the second letter mapping of collection operations

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,9 +1,20 @@
 'use strict';
 
 function average_to_letter(collection) {
+  var res = (collection.reduce((pre, cur) => pre + cur))/collection.length;
+  return generateString(Math.ceil(res));
+}
 
-  //在这里写入代码
+function generateString(index) {
+  var excess = Math.floor((index - 1) / 26);
+  var basic = 96 + index;
+  var former = 96 + excess;
+  var latter = 96 + index - excess * 26;
+  if (!excess) {
+    return String.fromCharCode(basic);
+  } else {
+    return String.fromCharCode(former) + String.fromCharCode(latter);
+  }
 }
 
 module.exports = average_to_letter;
-

e987a8064db93a70190dd1f2a6ade2c8987d293e
GraceeFlower <1491644103@qq.com>
2019-12-30 13:18:04 +0800

    [WangMin] feat-2: realize flatten about double dimensions to one & delete the repeat items

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  var res =  collection.reduce((pre, cur) => pre.concat(cur), []);
+  return res.filter((value, index, array) => array.indexOf(value) === index);
 }
 
 module.exports = double_to_one;
+double_to_one([[1,2,3],[5,2,1,4], [2,1]]);

bff87563725dbe12a82f98f3b24b45b0b87ac572
GraceeFlower <1491644103@qq.com>
2019-12-30 21:38:36 +0800

    [WangMin] feat-1: solve the third deleting the same elements object in section_3

--- main/section_3/practice_3/practice.js
+++ main/section_3/practice_3/practice.js
@@ -1,5 +1,36 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  collection_a = count_same_elements(collection_a);
+  object_b.value.map((item) => {
+    var index = checkItem(collection_a, item);
+    var count = collection_a[index].count;
+    collection_a[index].count -= Math.floor(count / 3);
+  });
+  return collection_a;
+}
+
+function count_same_elements(collection) {
+  var results = [];
+  collection.filter(function (value) {
+    var item = checkItem(results, value);
+    if (item !== "") {
+      results[item].count++;
+    } else {
+      var result = {};
+      result.key = value;
+      result.count = 1;
+      results.push(result);
+    }
+  })
+  return results;
+}
+
+function checkItem(arr, element) {
+  for(var item = 0; item < arr.length; item++) {
+    if(arr[item].key === element) {
+      return item;
+    }
+  }
+  return ""; 
 }
 
 module.exports = create_updated_collection;

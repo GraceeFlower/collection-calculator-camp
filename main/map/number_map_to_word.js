042de0f79af1c79c81a6ccf498fb2a747d3d4bf4
GraceeFlower <1491644103@qq.com>
2019-12-30 13:48:33 +0800

    [WangMin] feat-5: use map to change the item to required letter

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,10 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map((item) => generateString(item));
 };
+
+function generateString(item) {
+  return String.fromCharCode(96 + item);
+}
 
 module.exports = number_map_to_word;

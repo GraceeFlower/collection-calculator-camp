d5f120061fa92eb8b234eb94f8302f6ed872c973
GraceeFlower <1491644103@qq.com>
2019-12-30 15:30:25 +0800

    [WangMin] feat-6: use reduce & sort to compute the median

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  collection.sort((pre, cur) => pre - cur);
+  var numLen = collection.length;
+  if (numLen % 2) {
+    return collection[(numLen - 1) / 2];
+  } else {
+    return (collection[numLen / 2 - 1] + collection[numLen / 2]) / 2;
+  }
 }
 
 module.exports = compute_median;

0503ede7dda491e1dcb6e851698b26d0dc98a03a
GraceeFlower <1491644103@qq.com>
2019-12-30 23:28:52 +0800

    [WangMin] feat-4: solve the forth judging is exist element of subscript is even

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,7 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  var res = collection.filter((value, index) => index % 2 === 0);
+  return res.includes(element);
 };
+
 module.exports = is_exist_element;

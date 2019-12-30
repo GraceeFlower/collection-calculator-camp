8ce507ecf06ba3b56027fc11e2f8d0e69ddc1d81
GraceeFlower <1491644103@qq.com>
2019-12-30 22:32:04 +0800

    [WangMin] feat-1: solve the first calculating average of subscript is even

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,7 @@
 'use strict';
 var calculate_average = function(collection){
-
+  var res = collection.filter((item) => item % 2 === 0);
+  return res.reduce((pre, cur) => pre + cur) / res.length;
 };
+
 module.exports = calculate_average;

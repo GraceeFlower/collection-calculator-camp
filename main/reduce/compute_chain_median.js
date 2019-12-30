faa01e6e4d909a0405f8273457a39887bae4b6fe
GraceeFlower <1491644103@qq.com>
2019-12-30 15:18:14 +0800

    [WangMin] feat-5: use reduce & sort to compute the chain median

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  var array = collection.split("->").map((item) => parseInt(item));
+  array.sort((pre, cur) => pre - cur);
+  var numLen = array.length;
+  if (numLen % 2) {
+    return array[(numLen - 1) / 2];
+  } else {
+    return (array[numLen / 2 - 1] + array[numLen / 2]) / 2;
+  }
 }
 
 module.exports = compute_chain_median;

25b079f283572498cf3f1ed38da79ea52c47007c
GraceeFlower <1491644103@qq.com>
2019-12-30 16:52:12 +0800

    [WangMin] feat-12: use reduece to split to zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  var res = [];
+  for(var item = number; item > 0;) {
+    var nextItem = (item * 10 - interval * 10) / 10;
+    res.push(item);
+    if (nextItem <= 0) {
+      res.push(nextItem);
+    }
+    item = nextItem;
+  }
+  return res;
 }
 
 module.exports = spilt_to_zero;

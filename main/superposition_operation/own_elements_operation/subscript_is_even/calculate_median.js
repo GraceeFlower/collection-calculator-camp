74e31375ffc9bf7a8e93b7b7a26b51e712911f51
GraceeFlower <1491644103@qq.com>
2019-12-30 22:34:56 +0800

    [WangMin] feat-2: solve the second calculating medium of subscript is even

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,12 @@
 'use strict';
 var calculate_median = function(collection){
-
+  var res = collection.filter((item) => item % 2 === 0);
+  var numLen = res.length;
+  if (numLen % 2) {
+    return res = res[(numLen - 1) / 2];
+  } else {
+    return res =  (res[numLen / 2 - 1] + res[numLen / 2]) / 2;
+  }
 };
+
 module.exports = calculate_median;

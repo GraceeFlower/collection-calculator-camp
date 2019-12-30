e1fa0ff6c17b78bace232441c7e527a0f47e06b3
GraceeFlower <1491644103@qq.com>
2019-12-30 21:50:50 +0800

    [WangMin] feat-1: solve the first letter mapping of collection operations

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function even_to_letter(collection) {
+  var res = collection.filter((item) => item % 2 === 0);
+  return res.map((item) => item = generateString(item));
+}
 
-  //在这里写入代码
+function generateString(index) {
+  var excess = Math.floor((index - 1) / 26);
+  var basic = 96 + index;
+  var former = 96 + excess;
+  var latter = 96 + index - excess * 26;
+  if (!excess) {
+    return String.fromCharCode(basic);
+  } else {
+    return String.fromCharCode(former) + String.fromCharCode(latter);
+  }
 }
 
 module.exports = even_to_letter;
+ 

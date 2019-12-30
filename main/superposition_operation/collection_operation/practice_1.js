bf328afe4da0d5897389ec3e9f1562ed7c451d23
GraceeFlower <1491644103@qq.com>
2019-12-30 21:57:34 +0800

    [WangMin] feat-3: solve the third letter mapping of collection operations

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function hybrid_operation(collection) {
-
-  //在这里写入代码
+  var res = collection.map((item) => item * 3 + 2);
+  return res.reduce((pre, cur) => pre + cur);
 }
 
 module.exports = hybrid_operation;
-

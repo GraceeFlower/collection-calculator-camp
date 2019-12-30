9e68d89f96bfaac4551fb96ab28f9000673f710b
GraceeFlower <1491644103@qq.com>
2019-12-30 16:04:03 +0800

    [WangMin] feat-11: use reduce to find the last even of given array

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  var item = 0;
+  var res = null;
+  while(item < collection.length) {
+    if (collection[item] % 2) {
+      item++;
+    } else {
+      res = collection[item];
+      item++;
+    }
+  }
+  return res;
 }
 
 module.exports = find_last_even;

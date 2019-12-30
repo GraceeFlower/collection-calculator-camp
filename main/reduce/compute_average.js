638c0fa9efd4da97caa031d09c127a984e211d8d
GraceeFlower <1491644103@qq.com>
2019-12-30 14:50:14 +0800

    [WangMin] feat-4: use reduce to compute the average of all the items in given array

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+  return collection.reduce((pre, cur) => pre + cur)/collection.length;
 }
 
 module.exports = compute_average;
-

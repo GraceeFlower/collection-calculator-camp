66b428226554a1510ca477541aadecc84f1fcacd
GraceeFlower <1491644103@qq.com>
2019-12-30 17:25:36 +0800

    [WangMin] feat-2: solve the second collecting same elements in section_1

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var totalArr = collection_b.concat(collection_a);
+  totalArr = double_to_one(totalArr);
+  var res = totalArr.filter(function (value, index, array) {
+    return array.indexOf(value) !== index;
+  }, collection_b.length);
+  return res;
+}
+
+function double_to_one(collection) {
+  return collection.reduce((pre, cur) => pre.concat(cur), []);
 }
 
 module.exports = collect_same_elements;

21cca74f1985a8832551f440b4a9dc0b046804f1
GraceeFlower <1491644103@qq.com>
2019-12-30 22:10:09 +0800

    [WangMin] feat-5: solve the fifth letter mapping of collection operations

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  var res = collection.filter((item) => item % 2 !== 0);
+  res = res.map((item) => item * 3 + 5);
+  return res.reduce((pre, cur) => pre + cur);
 }
 
 module.exports = hybrid_operation_to_uneven;
-

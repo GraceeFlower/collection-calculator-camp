a99f06ceae1c5995f01bd8ee945f81f42c757dd2
GraceeFlower <1491644103@qq.com>
2019-12-31 10:32:50 +0800

    [WangMin] feat-2: solve the second collectiong one_add_next_multiply_three of own elements operation

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,13 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+  var res = [];
+  for(var item = 0; item < collection.length; item++) {
+    if(item + 1 !== collection.length) {
+      var multi = (collection[item] + collection[item + 1]) * 3;
+      res.push(multi);
+    }
+  }
+  return res;
 }
+
 module.exports = one_add_next_multiply_three;

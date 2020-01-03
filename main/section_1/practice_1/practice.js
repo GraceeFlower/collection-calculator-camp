b81e75867aa506acae310f471c538ec6c1d1353d
GraceeFlower <1491644103@qq.com>
2020-01-04 00:19:44 +0800

    [WangMin] fix-1: delete the useless params 'res' & return the result directly of practice_1 in section_1 folder

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,9 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
   var totalArr = collection_b.concat(collection_a);
-  var res = totalArr.filter(function (value, index, array) {
+  return totalArr.filter(function (value, index, array) {
     return array.indexOf(value) !== index;
   }, collection_b.length);
-  return res;
 }
 
 module.exports = collect_same_elements;

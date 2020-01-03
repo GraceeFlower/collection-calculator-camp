31dfe89e7c9331d5b6ead89261e886678938d688
GraceeFlower <1491644103@qq.com>
2020-01-04 00:04:12 +0800

    [WangMin] fix-6: replace if-else sentence with ternary operator in collectionOperator folder

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -21,11 +21,8 @@ function generateString(index) {
   var basic = 96 + index;
   var former = 96 + excess;
   var latter = 96 + index - excess * 26;
-  if (!excess) {
-    return String.fromCharCode(basic);
-  } else {
-    return String.fromCharCode(former) + String.fromCharCode(latter);
-  }
+  return (excess ? String.fromCharCode(former) + String.fromCharCode(latter) :
+      String.fromCharCode(basic));
 }
 
 module.exports = get_letter_interval_2;

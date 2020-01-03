c3a536df3e8d21e70de165dfc30c8293749b53e2
GraceeFlower <1491644103@qq.com>
2020-01-04 00:50:19 +0800

    [WangMin] fix-2: delete the meaningless assignments about res in subscript_is_even folder

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -3,9 +3,9 @@ var calculate_median = function(collection){
   var res = collection.filter((value, index) => index % 2 !== 0);
   var numLen = res.length;
   if (numLen % 2) {
-    return res = res[(numLen - 1) / 2];
+    return res[(numLen - 1) / 2];
   } else {
-    return res =  (res[numLen / 2 - 1] + res[numLen / 2]) / 2;
+    return (res[numLen / 2 - 1] + res[numLen / 2]) / 2;
   }
 };
 

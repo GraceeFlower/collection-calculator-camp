e1fe89d803b1ad78f2230651fa82250f49b4a4bb
GraceeFlower <1491644103@qq.com>
2020-01-04 00:53:15 +0800

    [WangMin] fix-4: delete the meaningless assignments about res in subscript_is_even folder

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,7 +1,7 @@
 'use strict';
 var is_exist_element = function(collection,element){
-  var res = collection.filter((value, index) => index % 2 === 0);
-  return res.includes(element);
+  return collection.filter((value, index) => index % 2 === 0)
+  .includes(element);
 };
 
 module.exports = is_exist_element;

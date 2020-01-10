845b1e2d37f7727961680948a042300bcc45e75d
GraceeFlower <1491644103@qq.com>
2020-01-10 18:21:07 +0800

    [WangMin] refactor: use set to solve the deleting duplicate elements problem

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,15 +1,9 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  var res = collection_a.concat(collection_b);
-  for(var item = collection_a.length; item < res.length;) {
-    if (res.indexOf(res[item]) !== item) {
-      res.splice(item, 1);
-    } else {
-      item++;
-    }
-  }
-  return res;
+  let totalArr = collection_a.concat(collection_b);
+  let mySet = new Set(totalArr);
+  return [...mySet];
 }
 
 module.exports = get_union;
